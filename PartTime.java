14cf670009cd330277ee6a1d198ee7317bc0948f
Swapy78 <swapnilbabanmane@gmail.com>
2023-04-07 23:50:06 +0530

    Added Java program to calculate wage of part time employee

---
+++ PartTime.java
@@ -0,0 +1,21 @@
+package calculator;
+
+public class PartTime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int wagePerHour = 20;
+		int partTimeHours = 8;
+		
+		double isPresent = Math.random();
+		
+		int dailyWage = 20*8;
+		
+	
+			System.out.println("Daily wage of part time employee is "+dailyWage);
+		}
+	
+	
+	
+		
+	}
